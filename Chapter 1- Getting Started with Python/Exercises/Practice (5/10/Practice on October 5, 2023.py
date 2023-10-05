a3baa7de7060a47d354ff6535d77044186c01c77
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-10-05 17:53:25 +0400

    Practice (5/10/2023).py

---
+++ Chapter 1- Getting Started with Python/Exercises/Practice (5/10/Practice on October 5, 2023.py
@@ -0,0 +1,31 @@
+#User input
+Name = (input('Enter your name: '))
+
+Age = (input('Enter your age: '))
+
+Gender = (input('Enter your gender: '))
+
+Nationality = (input('Enter your Nationality: '))
+
+Birth_date = (input('Enter your Birth_date: '))
+
+Marital_Status = (input('Enter your Marital_Status: '))
+
+Address = (input('Enter your Address: '))
+
+Contact_Number = (input('Enter your Contact_Number: '))
+
+Email_Address = (input('Enter your Email_Address: '))
+
+Favourite_Color = (input('Enter your Favourite_Color: '))
+
+print(Name)
+print(Age)
+print(Gender)
+print(Nationality)
+print(Birth_date)
+print(Marital_Status)
+print(Address)
+print(Contact_Number)
+print(Email_Address)
+print(Favourite_Color)
