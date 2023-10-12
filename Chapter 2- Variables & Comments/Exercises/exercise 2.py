a705e86ef3c2a9efa9bc52801aba6ecaf7092cf4
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-10-12 13:27:27 +0400

    exercise 2.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 2.py
@@ -0,0 +1,7 @@
+#Exercise 2: Variables (Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks: Albert Einstein once said, “A person who never made a mistake never tried anything new.)
+
+Famous_person = "Dr. Jose Rizal"
+
+message = f'{Famous_person} once said, "The youth is the hope of our future."'
+
+print(message)
