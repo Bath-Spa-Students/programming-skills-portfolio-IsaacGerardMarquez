0185314c6be9d229ed7337be82c905f739deaa96
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-10-12 13:28:52 +0400

    exercise 4.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 4.py
@@ -0,0 +1,5 @@
+#Exercise 4: Favorite Number (Use a variable to represent your favorite number. Then,using that variable, create a message that reveals your favorite number. Print that message.)
+
+Favourite_Number = 0
+Message = f"My favourite number is {Favourite_Number}."
+print (Message)
