2b6595e331b64fce16578f76ba086e58500faaef
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-10-12 13:25:45 +0400

    exercise 1.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 1.py
@@ -0,0 +1,7 @@
+#Exercise 1: Variables (Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.)
+
+message = "Learning Python is challenging yet amusing."
+print(message)
+
+message = "It's truly enjoyable!"
+print(message)
