175829832d44d95c9499c8de71e86254cb1f9f95
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-10-24 16:58:18 +0400

    exercise 3.py

---
+++ Chapter 4- Control Flow/Exercises/exercise 3.py
@@ -0,0 +1,35 @@
+# Exercise 3: Alien Colors #3 (Turn your if-else chain from Exercise 5-4 into an if-elifelse chain.)
+# • If the alien is green, print a message that the player earned 5 points.
+# • If the alien is yellow, print a message that the player earned 10 points.
+# • If the alien is red, print a message that the player earned 15 points.
+# • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+
+# Program-1-Green
+alien_color = "green"
+
+if alien_color == "green":
+    print ("Congratulations! You just earned 5 points!")
+elif alien_color == "yellow":
+    print ("Congratulations! You just earned 10 points!")
+else:
+    print ("Congratulations! You just earned 15 points!")
+
+# Program-2-Yellow
+alien_color = "yellow"
+
+if alien_color == "green":
+    print ("Congratulations! You just earned 5 points!")
+elif alien_color == "yellow":
+    print ("Congratulations! You just earned 10 points!")
+else:
+    print ("Congratulations! You just earned 15 points!")
+
+# Program-3-Red
+alien_color = "red"
+
+if alien_color == "green":
+    print ("Congratulations! You just earned 5 points!")
+elif alien_color == "yellow":
+    print ("Congratulations! You just earned 10 points!")
+else:
+    print ("Congratulations! You just earned 15 points!")
