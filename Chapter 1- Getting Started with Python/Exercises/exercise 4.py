29de1b0cdcd20f3679fec019435e44cd38957b8f
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-10-28 15:26:45 +0400

    exercise 4.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 4.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 4.py
@@ -1,8 +1,10 @@
 #Exercise 4: Strings Concatination (Write three strings in different variables and print the output as one string.)
 
+# Three strings in different variables.
 String1 = "Python"
 String2 = "is"
 String3 = "captivating."
 
+# Printing the output as one string.
 New_string= String1 + " " + String2 + " " + String3
 print(New_string)
