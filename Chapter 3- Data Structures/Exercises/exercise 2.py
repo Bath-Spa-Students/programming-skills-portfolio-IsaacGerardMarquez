17e8993c5a76ea1d78327fc4069f40e2cc3e0360
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-10-28 15:41:52 +0400

    exercise 2.py

--- Chapter 3- Data Structures/Exercises/exercise 2.py
+++ Chapter 3- Data Structures/Exercises/exercise 2.py
@@ -1,7 +1,9 @@
 # Exercise 2: Greetings (Start with the list you used in Exercise 1, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.)
 
+# Creating a list called Names that would store the names of a few of my friends.
 Names = ['Yyan', 'John', 'Radjn']
 
+## Printing each person with the same message but each personalized with the person’s name.
 Message = f"Long time no see, {Names[0].title()}!"
 print (Message)
 
