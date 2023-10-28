1a92ef3977dc3f4d6083cd421fb02cc778e2a7eb
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-10-28 15:38:54 +0400

    exercise 1.py

--- Chapter 3- Data Structures/Exercises/exercise 1.py
+++ Chapter 3- Data Structures/Exercises/exercise 1.py
@@ -1,7 +1,9 @@
 #Exercise 1: Names (Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a time.)
 
+# Creating a list called Names that would store the names of a few of my friends.
 Names = ['Yyan', 'John', 'Radjn']
 
+# Printing each person’s name by accessing each element in the list, one at a time.
 print (Names[0])
 print (Names[1])
 print (Names[2])
