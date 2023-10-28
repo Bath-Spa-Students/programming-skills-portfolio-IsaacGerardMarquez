eef2ce156770f560bd99a0bb5254be7191cfd5aa
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-10-28 15:29:39 +0400

    practice 5.py

--- Chapter 1- Getting Started with Python/Exercises/practice 5.py
+++ Chapter 1- Getting Started with Python/Exercises/practice 5.py
@@ -1,8 +1,11 @@
 #Write four string in different variables and print the output as one string. (String Concatenation)
+
+# Four strings in different variables.
 string1 = "Good morning!"
 string2 = "My"
 string3 = "Name is"
 string4 = "Isaac Marquez."
 
+# Printing the output as one string.
 new_string= string1 + " " + string2 + " " + string3 + " " + string4
 print(new_string)
