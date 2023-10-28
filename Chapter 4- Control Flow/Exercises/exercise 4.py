eafac163416b5a188048ca54b66eda9569635e81
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-10-28 16:12:53 +0400

    exercise 4.py

--- Chapter 4- Control Flow/Exercises/exercise 4.py
+++ Chapter 4- Control Flow/Exercises/exercise 4.py
@@ -6,6 +6,7 @@
 # •If the person is at least 20 years old but less than 65, print a message that the person is an adult.
 # •If the person is age 65 or older, print a message that the person is an elder.
 
+# Setting a value for the variable Age
 Age = 18
 
 if Age < 2:
