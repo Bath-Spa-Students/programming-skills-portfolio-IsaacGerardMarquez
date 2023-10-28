f3e2fda07adcda6ed720dbcb6a9dd67391bfb38a
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-10-28 15:55:25 +0400

    exercise 5.py

--- Chapter 3- Data Structures/Exercises/exercise 5.py
+++ Chapter 3- Data Structures/Exercises/exercise 5.py
@@ -3,9 +3,10 @@
 # •Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
 # •Print a second set of invitation messages, one for each person who is still in your list.
 
-# The people that I invited to the dinner.
+# Creating a list that includes three people that I would like to invite to dinner.
 My_Guests = ["Justin Vasquez", "Arthur Nery", "Adie"]
 
+# With the use of the list, print a message to each person, inviting them to dinner.
 Name = My_Guests[0].title()
 print(f"{Name}, I would like to invite you to a dinner at my place.")
 
@@ -15,15 +16,16 @@ print(f"{Name}, I would like to invite you to a dinner at my place.")
 Name = My_Guests[2].title()
 print(f"{Name}, I would like to invite you to a dinner at my place.")
 
-
+# Printing a message that one guest (Arthur Nery) would not be able to come to the dinner.
 Name = My_Guests[1].title()
 print(f"\nUnfortunately, {Name} would not be able to come to the dinner.")
 
 # Arthur Nery would not be able to come to the dinner. Let us invite Kyle Echarri instead.
+# Replace the name of the guest that can't make it with the name of the new person I am inviting.
 del(My_Guests[1])
 My_Guests.insert(1, 'Kyle Echarri')
 
-# Print the invitations again.
+# Print a second set of invitation messages, one for each person who is still in your list.
 Name = My_Guests[0].title()
 print(f"\n{Name}, I would like to invite you to a dinner at my place.")
 
