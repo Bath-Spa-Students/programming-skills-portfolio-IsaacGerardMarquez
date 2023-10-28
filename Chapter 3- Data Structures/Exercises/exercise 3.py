43a7dda3976fee679dcba8bd2a9eb6bf9ae406c6
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-10-28 15:46:48 +0400

    exercise 3.py

--- Chapter 3- Data Structures/Exercises/exercise 3.py
+++ Chapter 3- Data Structures/Exercises/exercise 3.py
@@ -1,6 +1,8 @@
 # Exercise 3: Your Own List (Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”)
 
+# Creating a list that stores several examples of car I would like to someday be able to own and drive.
 Cars = ["Honda Civic", "Dodge Charger", "Ford Raptor", "BMW X6", "Mini Cooper", "Tesla Model Y", "Kia Stinger"]
 
+# Using the list created, print a series of statements about these items.
 for Cars in Cars:
     print(f"I would like to someday be able to own and drive a {Cars}.")
