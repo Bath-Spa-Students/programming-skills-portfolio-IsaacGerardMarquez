857956da4ebefccc63487a33db11f3a11995cc9a
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-10-30 18:42:35 +0400

    exercise 2.py

--- Chapter 6- Loops/Exercises/exercise 2.py
+++ Chapter 6- Loops/Exercises/exercise 2.py
@@ -1,6 +1,6 @@
 # Exercise 2: Movie Tickets: (A movie theater charges different ticket prices depending on a person’s age. If a person is under the age of 3, the ticket is free; if they are between 3 and 12, the ticket is $10; and if they are over age 12, the ticket is $15. Write a loop in which you ask users their age, and then tell them the cost of their movie ticket)
 
-# Loop that asks the users their age, and then tell them the cost of their movie ticket. 
+# Loop that asks the users their age, and then tell them the cost of their movie ticket. It will only end when the user enter the 'quit' value.
 prompt = "Please enter your age :"
 prompt += "\nEnter 'quit' when you are finished: "
 
@@ -16,3 +16,5 @@ while True:
         print("  The movie ticket will cost you $10.")
     else:
         print("  The movie ticket will cost you $15.")
+
+print ("Thank you and enjoy your movie!")
