1f7f14d0800ae77865ca73056dccc2e92930b138
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-10-30 09:34:02 +0400

    exercise 1.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise 1.py
@@ -0,0 +1,15 @@
+# Exercise 1: Person (Use a dictionary to store information about a person you know. Store their first name, last name, age, and the city in which they live. You should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your dictionary.)
+
+# Storing information about a person that I know.
+Person_I_Know = {
+    'First_Name': "Elijah",
+    "Last_Name": "Marquez",
+    "Age": 10,
+    "City": "Sharjah"
+}
+
+# Print the information about the person that I know that was stored in my dictionary.
+print (Person_I_Know["First_Name"])
+print (Person_I_Know["Last_Name"])
+print (Person_I_Know["Age"])
+print (Person_I_Know["City"])
