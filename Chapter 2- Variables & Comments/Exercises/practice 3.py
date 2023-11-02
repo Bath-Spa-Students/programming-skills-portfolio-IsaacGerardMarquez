b1b2f4ed3d67bb1c44b99024762845e28717e8f8
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-02 13:47:32 +0400

    practice 3.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice 3.py
@@ -0,0 +1,10 @@
+#Q3: Write a python program that takes an input 5 from user and then type cast those values to string, int and float in the separate variables. Print all the variables.
+
+# Creating variables
+String = str(5)
+Integer = int(5)
+Float = float(5)
+
+print(String)
+print(Integer)
+print(Float)
