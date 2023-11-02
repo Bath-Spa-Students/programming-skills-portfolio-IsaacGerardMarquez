9c6e4fb2358152e6c73114e6bbb86ef4845b88ab
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-02 13:48:05 +0400

    practice 4.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice 4.py
@@ -0,0 +1,9 @@
+#Q4: Write a python program that stores an integer and string value to variables x and y. Print the type of variable x and y.
+
+# Creating variables that stores an integer and string value.
+x = 1
+y = "One"
+
+# Printing the type of variable that x and y stores
+print(type(x))
+print(type(y))
