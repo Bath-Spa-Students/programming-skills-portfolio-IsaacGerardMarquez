e83b3dc8d3bc8c73df2e35075993cf790581af9e
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-06 22:29:42 +0400

    exercise 3.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 3.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 3.py
@@ -1,15 +1,22 @@
 #Exercise 3: Print date and Time (Write a Python program to display the current date and time.)
 
+# Use import datetime to import the datetime module.
 import datetime
 
+# Creating a variable named "time".
+# Used the datetime.now() to retrieve the current date and time.
 time = datetime.datetime.now()
 
+# Using the print() function to print or display the output.
 print("The current date is ...")
 print(time.strftime("%y / %m / %d"))
 print("The current time is ...")
 print(time.strftime("%I / %M / %S"))
 print(" ")
+
 print("Or simply")
+
 print(" ")
+# Using the print () function to display the value stored in the variable named "time", which represents the current date and time.
 print (time)
 print(" ")
