fba490cb390e6362264f9e8a9d50bed08a03c644
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-06 22:04:15 +0400

    exercise 1.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 1.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 1.py
@@ -1,5 +1,6 @@
 #Exercise 1: Print Strings (Write a Python program to print the following string in a specific format. Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are)
 
+# In this Python Program, I have used triple quotation marks ("""") to print a multi-line string.
 print ("""Twinkle, twinkle, little star,
 	How I wonder what you are! 
 		Up above the world so high,   		
