1958bc2ec3ed26f2547503c0c811003ce1d8c1d3
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-06 22:47:32 +0400

    exercise 2.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 2.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 2.py
@@ -1,7 +1,15 @@
 #Exercise 2: Print the Version of Python (Write a Python program to get the Python version you are using.)
 
-import sys  
+# Use import sys to import the sys module in the Python Program.
+import sys
+
+# Use the print() function to print the string "The Python version that I am using is: ".
 print("The Python version that I am using is:")
+# Use "sys.version" to access a string that represents the Python version that I am currently using.
 print (sys.version)
-print("Version info.")
+
+# Use the print() function to print the string "Version info: ".
+print("Version info:")
+
+# Use "sys.version_info" to access the components of the Python version that I am currently using.
 print (sys.version_info)    
