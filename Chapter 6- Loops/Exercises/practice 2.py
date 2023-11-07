2940fb7bb99f29caea9f9c86347e041b2fd1e86f
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-08 01:31:04 +0400

    practice 2.py

---
+++ Chapter 6- Loops/Exercises/practice 2.py
@@ -0,0 +1,8 @@
+# Practice 2: Write a Python program that uses a for loop to print the numbers from 1 to 10.
+
+print("The Numbers from 1 to 10 are: ")
+# Creating a program that would take a range from 1 to 11.
+for n in range(1, 11):
+   
+# Printing or displaying all numbers in an interval 1 to 11 using the For Loop.
+   print(n, end=' ')
