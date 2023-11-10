f72f4828357a513e039504df4fd379a6763535a0
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-10 11:10:11 +0400

    practice 4.py

---
+++ Chapter 3- Data Structures/Exercises/practice 4.py
@@ -0,0 +1,9 @@
+# Practice 4: Assume the lists numbers1 has 100 elements and numbers2 is an empty list. Write code that copies the values in numbers to numbers2.
+
+# Creating code that would make the list numbers1 have 100 items. This statement creates a list that has 100 elements (the integer 16).
+numbers1 = [16]*100
+# Assigning the elements in the list named numbers1 to the list named numbers2 (with the use of full colon).
+numbers2 = numbers1[:]
+
+# Use print() function to print or display the values in numbers1 going to numbers2.
+print(numbers2)
