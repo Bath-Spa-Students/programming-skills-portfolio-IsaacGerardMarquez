cfe363802818e0859df0f9757e1fad15c40490bb
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-10 12:53:02 +0400

    practice 4.py

---
+++ Chapter 5- Dictionaries/Exercises/practice 4.py
@@ -0,0 +1,24 @@
+# Practice 4: Write a Python program to iterate through both the keys and values of a dictionary and print them.
+
+# I created a dictionary named "Isaac_Introduction" that stores information about myself.
+Isaac_Introduction = {
+    'Name': 'Isaac Gerard S. Marquez',
+    'Nationality': 'Filipino',
+    'Languages Spoken': ['English', 'Filipino', 'Hiligaynon'],
+    'Age': 18,
+    'Date of Birth' : 'January 25, 2005',
+    'Sex': 'Male',
+    'Address': "Al Qulaya'ah, Sharjah",
+    'Contact Number': '971566630388',
+    'Email Address': 'IsaacBathSpa@gmail.com',
+    'Hobbies': ['Playing guitar', 'singing', 'cooking', 'photography', 'playing sports such as basketball and volleyball', 'learning'],
+    'Pet': 'Maltipoo',
+    'University': 'Bath Spa University RAK',
+    'Current Level': 'Level 4 (Year 1)',
+}
+
+# Use for loop and "items()"to iterate through both the keys and values of the dictionary named "Isaac_Introduction".
+for key, value in Isaac_Introduction.items():
+    
+# Use print() function to print or display each "key" and "value" on a separate line.
+    print(key, value)
