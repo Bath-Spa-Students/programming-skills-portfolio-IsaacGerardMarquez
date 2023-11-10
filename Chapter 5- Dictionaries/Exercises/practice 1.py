b513656ccf695bddce50d8229348fad9f0dd6cde
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-10 12:18:07 +0400

    practice 1.py

---
+++ Chapter 5- Dictionaries/Exercises/practice 1.py
@@ -0,0 +1,17 @@
+# Practice 1: Use a dictionary to store information about yourself.
+
+# I created a dictionary named "Isaac_Introduction" that stores information about myself.
+Isaac_Introduction = {
+    'Name': 'Isaac Gerard S. Marquez',
+    'Nationality': 'Filipino',
+    'Age': 18,
+    'Date of Birth' : 'January 25, 2005',
+    'Sex': 'Male',
+    'Address': "Al Qulaya'ah, Sharjah",
+    'Contact Number': '971566630388',
+    'Email Address': 'IsaacBathSpa@gmail.com',
+    'Hobbies': ['Playing guitar', 'singing', 'cooking', 'photography', 'playing sports such as basketball and volleyball', 'learning'],
+    'Pet': 'Maltipoo',
+    'University': 'Bath Spa University RAK',
+    'Current Level': 'Level 4 (Year 1)',
+}
