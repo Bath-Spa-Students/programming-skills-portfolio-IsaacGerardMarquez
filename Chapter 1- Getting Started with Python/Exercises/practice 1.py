810096b4d5b37e1dfcc101228c5a97307afc9083
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-10 14:08:00 +0400

    practice 1.py

--- Chapter 1- Getting Started with Python/Exercises/practice 1.py
+++ Chapter 1- Getting Started with Python/Exercises/practice 1.py
@@ -1,5 +1,7 @@
 #Write a program to output the letters W X Y Z, each on a separate line
-print("""W 
+
+# In this program, I wrote the letters W X Y Z within the triple quotes (""") in order to print them each on a separate line.
+print("""W
 X 
 Y 
 Z""")
