8224ea8096d63937f7739fa74d469d732568d522
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-10 14:26:32 +0400

    practice 2.py

--- Chapter 1- Getting Started with Python/Exercises/practice 2.py
+++ Chapter 1- Getting Started with Python/Exercises/practice 2.py
@@ -1,2 +1,7 @@
-#Create a program to output "hi"50 times, without any separator
+#Create a program to output "hi" 50 times, without any separator
+
+# In this program, "hi" is a string that represents the word "hi".
+# "*" is used as the operator for string repetition.
+# In this instance, "hi"*50 will concatenate the string "hi" together 50 times.
+# Lastly, the print() function will then output or display "hi" 50 times, without any separator.
 print ("hi"*50)
