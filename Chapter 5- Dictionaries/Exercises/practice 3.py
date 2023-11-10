938fe96542788d92e94a644c3a087f73fba2d89a
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-10 12:35:52 +0400

    practice 3.py

---
+++ Chapter 5- Dictionaries/Exercises/practice 3.py
@@ -0,0 +1,24 @@
+# Practice 3: Write a Python program to iterate through the values of a dictionary and print them.
+
+# I created a dictionary named "Isaac_Introduction" that stores information about myself.
+Isaac_Introduction = {
+    'Name': 'Isaac Gerard S. Marquez',
+    'Nationality': 'Filipino',
+    'Languages Spoken': ['English', 'Filipino', 'Hiligaynon'],
+    'Age': 18,
+    'Date of Birth' : 'January 25, 2005',
+    'Sex': 'Male',
+    'Address': "Al Qulaya'ah, Sharjah",
+    'Contact Number': '971566630388',
+    'Email Address': 'IsaacBathSpa@gmail.com',
+    'Hobbies': ['Playing guitar', 'singing', 'cooking', 'photography', 'playing sports such as basketball and volleyball', 'learning'],
+    'Pet': 'Maltipoo',
+    'University': 'Bath Spa University RAK',
+    'Current Level': 'Level 4 (Year 1)',
+}
+
+# Use for loop to iterate through the values of the dictionary named "Isaac_Introduction".
+for value in Isaac_Introduction.values():
+    
+# Use print() function to print or display each "value" on a separate line.
+    print(value)
