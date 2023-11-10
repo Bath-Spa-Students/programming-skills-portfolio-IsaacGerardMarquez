3b41adb26e54e094c9f2d5db281956cea463d0a9
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-10 14:51:26 +0400

    practice 3.py

--- Chapter 1- Getting Started with Python/Exercises/practice 3.py
+++ Chapter 1- Getting Started with Python/Exercises/practice 3.py
@@ -1,17 +1,9 @@
-#You need to make a program for a leader board. The program needs to output the numbers 1 to 9, each on a separate line followed by a dot
-print("""
-1.
-2.
-3.
-4.
-5.
-6.
-7.
-8.
-9.
-""")
-print ("")
-print ("or")
-print ("")
-for i in range(9):
-    print(str(i + 1) + ".")
+# Practice 3: You need to make a program for a leader board. The program needs to output the numbers 1 to 9, each on a separate line followed by a dot.
+
+# Create a for loop to iterate over the range of numbers from 1 to 10. The loop variable named "leaderboard" will then take on the values from 1 to 9 in each iteration.
+for leaderboard in range(1, 10):
+
+# Create a print() function to display or print the value of number.
+# The "end" parameter is set to ".\n", which means that there will be a dot (.) printed after each number.
+# The new line character, “\n”, is used so that each number will be printed each on a separate line.
+    print(leaderboard, end=".\n")
