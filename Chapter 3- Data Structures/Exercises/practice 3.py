f6b40d8a25e86590094cef9934f3ddfbd49cff14
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-10 11:04:56 +0400

    practice 3.py

---
+++ Chapter 3- Data Structures/Exercises/practice 3.py
@@ -0,0 +1,10 @@
+# Assume names references a list. Write a for loop that displays each element of the list.
+
+# Creating a list named Famous_Mathematicians that stores the following strings: 'Einstein', Newton' , 'Copernicus', and 'Kepler'
+Famous_Mathematicians = ['Einstein', 'Newton' , 'Copernicus', 'Kepler']
+
+# Use for loop to iterate over each element in the list named "Famous_Mathematician". 
+for Famous_Mathematician in Famous_Mathematicians:
+
+# Use print() function to print or display the value of "Famous_Mathematician".
+    print (Famous_Mathematician)
