1c4764e6237daac9907b665180f9f40876291b26
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-10 11:01:30 +0400

    practice 2.py

---
+++ Chapter 3- Data Structures/Exercises/practice 2.py
@@ -0,0 +1,4 @@
+# Write a statement that creates a list with the following strings: 'Einstein', Newton' , 'Copernicus', and 'Kepler'
+
+# Creating a list named Famous_Mathematicians that stores the following strings: 'Einstein', Newton' , 'Copernicus', and 'Kepler'
+Famous_Mathematicians = ['Einstein', 'Newton' , 'Copernicus', 'Kepler']
