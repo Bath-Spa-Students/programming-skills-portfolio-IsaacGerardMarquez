e7f6a849613406464a12769b214adf1cb0ff2eac
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-10 13:19:55 +0400

    practice 1.py

---
+++ Chapter 4- Control Flow/Exercises/practice 1.py
@@ -0,0 +1,14 @@
+# Write a python program with if statement that assigns 20 to the variable y and assigns 40 to the variable z if the variable & is greater than 100.
+
+# Create a variable named "x" that has a value of 368
+x = 368
+
+# Create an if statement that would determine if the variable named "x" is greater than 100.
+# If it meets the condition (True), the code block inside the if statement is executed.
+if x > 100:
+    y = 20
+    z = 40
+
+# Create a print() function that uses f-string formatting to output the values of x, y, and z.
+print (f"y = {y}")
+print (f"z = {z}")
