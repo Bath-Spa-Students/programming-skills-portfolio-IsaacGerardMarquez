25cc222a8b84213cefb92a9f4a29281801cd1312
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-11 21:15:32 +0400

    practice 3.py

--- Chapter 2- Variables & Comments/Exercises/practice 3.py
+++ Chapter 2- Variables & Comments/Exercises/practice 3.py
@@ -1,10 +1,19 @@
 #Q3: Write a python program that takes an input 5 from user and then type cast those values to string, int and float in the separate variables. Print all the variables.
 
-# Creating variables
+# The str() function converts the value inside the parentheses (which is 5) into a string.
 String = str(5)
+
+# The int() function converts the value inside the parentheses (which is 5) into an integer.
 Integer = int(5)
+
+# The float() function converts the value inside the parentheses (which is 5) into a floating-point number.
 Float = float(5)
 
+# Using print() function to display the value of the variable "String".
 print(String)
+
+# Using print() function to display the value of the variable "Integer".
 print(Integer)
+
+# Using print() function to display the value of the variable "Float".
 print(Float)
