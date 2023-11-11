5044f36c8b4935558dea72e58dcd3571ddb68475
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-11 22:02:21 +0400

    practice 2.py

--- Chapter 4- Control Flow/Exercises/practice 2.py
+++ Chapter 4- Control Flow/Exercises/practice 2.py
@@ -1,4 +1,4 @@
-# Write an if-else statement that assigns O to the variable b if the variable a is less than 10. Otherwise, it should assign 99 to the variable b.
+# Practice 2: Write an if-else statement that assigns O to the variable b if the variable a is less than 10. Otherwise, it should assign 99 to the variable b.
 
 # Create a variable named "a" that stores a value of 39.
 a = 39
