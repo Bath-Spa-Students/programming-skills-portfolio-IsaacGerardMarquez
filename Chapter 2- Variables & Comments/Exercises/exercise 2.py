1ce2b109402bc3c66998d6489245d4423293180e
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-11 17:04:41 +0400

    exercise 2.py

--- Chapter 2- Variables & Comments/Exercises/exercise 2.py
+++ Chapter 2- Variables & Comments/Exercises/exercise 2.py
@@ -1,7 +1,10 @@
 #Exercise 2: Variables (Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks: Albert Einstein once said, “A person who never made a mistake never tried anything new.)
 
+# The variable named "Famous_person" stores a string value "Dr. Jose Rizal".
 Famous_person = "Dr. Jose Rizal"
 
-message = f'{Famous_person} once said, "The youth is the hope of our future."'
+# I used an f-string to include the value of "Famous_person" as part of the message.
+Message = f'{Famous_person} once said, "The youth is the hope of our future."'
 
-print(message)
+# Create a print() function to display or output the message.
+print(Message)
