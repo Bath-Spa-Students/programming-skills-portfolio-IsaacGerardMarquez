bc4a623abfa9da96350709f9ba44677d57dc9274
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-11 21:44:47 +0400

    exercise 3.py

--- Chapter 6- Loops/Exercises/exercise 3.py
+++ Chapter 6- Loops/Exercises/exercise 3.py
@@ -1,4 +1,8 @@
 # Exercise 3: Infinity (Write a loop that never ends, and run it. (To end the loop, press ctrl-C or close the window displaying the output.))
 
+# The while loop is set to execute indefinitely because the condition "True" is always true.
+# This creates an infinite loop that will  execute continuously until the loop is interrupted or terminated.
 while (True):
+
+# The print() function is used to print or display the string "My name is Isaac." on the console.
     print ("My name is Isaac.")
