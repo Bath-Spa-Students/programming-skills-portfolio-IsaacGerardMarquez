e36cc3c5e4b6bfc88a7cf959390ca287030c71da
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-11 22:01:55 +0400

    practice 1.py

--- Chapter 4- Control Flow/Exercises/practice 1.py
+++ Chapter 4- Control Flow/Exercises/practice 1.py
@@ -1,4 +1,4 @@
-# Write a python program with if statement that assigns 20 to the variable y and assigns 40 to the variable z if the variable & is greater than 100.
+# Practice 1: Write a python program with if statement that assigns 20 to the variable y and assigns 40 to the variable z if the variable & is greater than 100.
 
 # Create a variable named "x" that has a value of 368
 x = 368
