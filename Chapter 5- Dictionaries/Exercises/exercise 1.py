8a33374e5592a58c1399f5cc443acb3cc06fb777
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-15 13:22:07 +0400

    exercise 1.py

--- Chapter 5- Dictionaries/Exercises/exercise 1.py
+++ Chapter 5- Dictionaries/Exercises/exercise 1.py
@@ -2,14 +2,22 @@
 
 # Creating a dictionary named "Person_I_Know" that store information about a person that I know.
 Person_I_Know = {
-    'First_Name': "Elijah",
+    "First_Name": "Elijah",
     "Last_Name": "Marquez",
     "Age": 10,
     "City": "Sharjah"
 }
 
 # Print the information about the person that I know that was stored in my dictionary.
+
+# Create a print() function that would print the value associated with the key "First_Name" in the dictionary named "Person_I_Know".
 print (Person_I_Know["First_Name"])
+
+# Create a print() function that would print the value associated with the key "Last_Name" in the dictionary named "Person_I_Know".
 print (Person_I_Know["Last_Name"])
+
+# Create a print() function that would print the value associated with the key "Age" in the dictionary named "Person_I_Know".
 print (Person_I_Know["Age"])
+
+# Create a print() function that would print the value associated with the key "City" in the dictionary named "Person_I_Know".
 print (Person_I_Know["City"])
