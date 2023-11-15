0542f155666c70aa5fdde1880ed13f8197bb3dee
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-15 15:45:35 +0400

    exercise 2.py

--- Chapter 7- Functions/Exercises/exercise 2.py
+++ Chapter 7- Functions/Exercises/exercise 2.py
@@ -3,8 +3,12 @@
 # Call the function, making sure to include a book title as an argument in the function call.
 
 # Function that would print a message, such as One of my favorite books is Alice in Wonderland.
+# Defining a function and naming it "favorite_book". It also takes a one argument named "title".
 def favorite_book(title):
-    """Displaying or printing a message about my favorite book."""
+
+# Creating a print() function to print or display a message. 
+# The "{title}" part of the string is replaced with the actual value of the title argument when the function is called.
     print(f"One of my favorite books is {title}.")
 
+# Calls the function named "favorite_book" and passes the argument "Noli Me Tángere".
 favorite_book("Noli Me Tángere")
