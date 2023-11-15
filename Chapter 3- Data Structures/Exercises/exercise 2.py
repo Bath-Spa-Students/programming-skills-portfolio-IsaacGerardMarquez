31fd8c0a1b9c0d1a095845c82a1ac7e5fd970382
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-15 11:32:44 +0400

    exercise 2.py

--- Chapter 3- Data Structures/Exercises/exercise 2.py
+++ Chapter 3- Data Structures/Exercises/exercise 2.py
@@ -5,21 +5,21 @@ Names = ['Yyan', 'John', 'Radjn']
 
 # Printing each person with the same message but each personalized with the person’s name.
 
-# Creating a f-string and then assigning it to the variable named "Message". 
+# Creating an f-string and then assigning it to the variable named "Message". 
 # Use the {Names[0].title()}! to access the first element (index 0) from the list named "Names".
 Message = f"Long time no see, {Names[0].title()}!"
 
 # Creating a print() function to display the value of the variable named "Message". The personalized message is printed using the first name from the list, which is Yyan.
 print (Message)
 
-# Creating a f-string and then assigning it to the variable named "Message". 
+# Creating an f-string and then assigning it to the variable named "Message". 
 # Use the {Names[1].title()}! to access the first element (index 1) from the list named "Names".
 Message = f"Long time no see, {Names[1].title()}!"
 
 # Creating a print() function to display the value of the variable named "Message". The personalized message is printed using the second name from the list, which is John.
 print (Message)
 
-# Creating a f-string and then assigning it to the variable named "Message". 
+# Creating an f-string and then assigning it to the variable named "Message". 
 # Use the {Names[2].title()}! to access the first element (index 2) from the list named "Names".
 Message = f"Long time no see, {Names[2].title()}!"
 
