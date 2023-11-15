49167a16e76fc11cb5e785d00c80ae5dc53e0a63
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-15 12:20:29 +0400

    exercise 3.py

--- Chapter 4- Control Flow/Exercises/exercise 3.py
+++ Chapter 4- Control Flow/Exercises/exercise 3.py
@@ -5,31 +5,34 @@
 # • Write three versions of this program, making sure each message is printed for the appropriate color alien.
 
 # Program-1-Green
+# The variable named "alien_color" is assigned to have the value of "green".
 alien_color = "green"
 
-if alien_color == "green":
+if alien_color == "green": # Elif statement will run if the value of alien_color is equal to "green".
     print ("Congratulations! You just earned 5 points!")
-elif alien_color == "yellow":
+elif alien_color == "yellow": # Elif statement will run if the value of alien_color is equal to "yellow".
     print ("Congratulations! You just earned 10 points!")
-else:
+else: # Else statement will run if the value of alien_color is not equal to either "green" and "yellow".
     print ("Congratulations! You just earned 15 points!")
 
 # Program-2-Yellow
-alien_color = "yellow"
+# The variable named "alien_color" is assigned to have the value of "yellow".
+alien_color = "yellow"  
 
-if alien_color == "green":
+if alien_color == "green": # Elif statement will run if the value of alien_color is equal to "green".
     print ("Congratulations! You just earned 5 points!")
-elif alien_color == "yellow":
+elif alien_color == "yellow": # Elif statement will run if the value of alien_color is equal to "yellow".
     print ("Congratulations! You just earned 10 points!")
-else:
+else: # Else statement will run if the value of alien_color is not equal to either "green" and "yellow".
     print ("Congratulations! You just earned 15 points!")
 
 # Program-3-Red
+# The variable named "alien_color" is assigned to have the value of "red".
 alien_color = "red"
 
-if alien_color == "green":
+if alien_color == "green":  # Elif statement will run if the value of alien_color is equal to "green".
     print ("Congratulations! You just earned 5 points!")
-elif alien_color == "yellow":
+elif alien_color == "yellow": # Elif statement will run if the value of alien_color is equal to "yellow".
     print ("Congratulations! You just earned 10 points!")
-else:
+else: # Else statement will run if the value of alien_color is not equal to either "green" and "yellow".
     print ("Congratulations! You just earned 15 points!")
