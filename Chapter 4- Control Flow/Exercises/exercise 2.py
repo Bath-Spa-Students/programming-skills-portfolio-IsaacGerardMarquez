f3c32ea4eec4f4e5c195f6a2c6d7065df92155a4
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-15 12:05:53 +0400

    exercise 2.py

--- Chapter 4- Control Flow/Exercises/exercise 2.py
+++ Chapter 4- Control Flow/Exercises/exercise 2.py
@@ -4,15 +4,28 @@
 #•Write one version of this program that runs the if block and another that runs the else block.
 
 # Program that runs the if block
+# The variable named "alien_color" is assigned to have the value of "green".
 alien_color = "green"
+
+# Use "==" to determine if alien_color is equal to 'green'. The if block would only be executed if alien_color is equal to green.
 if alien_color == "green":
+# Since the condition is met (True), the if statement if executed, which prints the message "Congratulations on shooting the alien! You just earned 5 points!".
     print("Congratulations on shooting the alien! You just earned 5 points!")
+
+# If the condition was not met (False), the else statement will be executed, which prints the message "Congratulations! You just earned 10 points!".
 else:
     print("Congratulations! You just earned 10 points!")
 
+
 # Program that runs the else block
+# The variable named "alien_color" is assigned to have the value of "orange".
 alien_color = "orange"
+
+# Use "==" to determine if alien_color is equal to 'green'.
 if alien_color == "green":
+# If the condition is met (True), the if statement will be executed, which prints the message "Congratulations on shooting the alien! You just earned 5 points!".
     print("Congratulations on shooting the alien! You just earned 5 points!")
+
+# However, because the condition was not met (False), the else statement is executed, which prints the message "Congratulations! You just earned 10 points!".
 else:
     print("Congratulations! You just earned 10 points!")
