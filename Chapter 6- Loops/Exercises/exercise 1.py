2ba5c93253825436c7025cf4eb4f57c6ca0d0a49
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-15 16:48:35 +0400

    exercise 1.py

--- Chapter 6- Loops/Exercises/exercise 1.py
+++ Chapter 6- Loops/Exercises/exercise 1.py
@@ -1,15 +1,15 @@
 # Exercise 1: Pizza Toppings (Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value. As they enter each topping, print a message saying you’ll add that topping to their pizza.)
 
-# Creating a variable named "prompt" that contains a message that would be displayed to the user. 
+# Creating a variable named "Topping" that contains a message that would be displayed to the user. 
 # The message prompts the user to enter what toppings would be added to their pizza.
-prompt = "\nGood day! What topping would you like to be added to your pizza?"
+Topping = "\nGood day! What topping would you like to be added to your pizza?"
 # Creating a second prompt that prompts the user to enter the word "quit" when they are finished with entering the toppings they want on their pizza.
-prompt += "\nPlease enter 'quit' when you are finished: "
+Topping += "\nPlease enter 'quit' when you are finished: "
 
 # Create a while loop that will continue to run until the user enter the word "quit".
 while True:
 # Creating an input() function that prompts to get input from the user, which is then stored in the variable named "Pizza_Topping".
-    Pizza_Topping = input(prompt)
+    Pizza_Topping = input(Topping)
 
 # If the user input is not equal to 'quit', the program will continue to run.
     if Pizza_Topping != 'quit':
