7a6c4dc3227eaf2ed94577db88519eeebfc1b8ce
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-15 12:50:40 +0400

    exercise 4.py

--- Chapter 4- Control Flow/Exercises/exercise 4.py
+++ Chapter 4- Control Flow/Exercises/exercise 4.py
@@ -6,18 +6,29 @@
 # •If the person is at least 20 years old but less than 65, print a message that the person is an adult.
 # •If the person is age 65 or older, print a message that the person is an elder.
 
-# Setting a value for the variable Age
+# Assigning the value of 18 to the variable named "Age".
 Age = 18
 
+# If the variable named "Age" is less than 2, the message "This person is a baby." will be printed.
 if Age < 2:
     print ("This person is a baby.")
+
+# If the variable named "Age" is less than 4, the message "This person is a toddler." will be printed.
 elif Age <4:
     print ("This person is a toddler.")
+
+# If the variable named "Age" is less than 13, the message "This person is a kid." will be printed.
 elif Age < 13:
     print ("This person is a kid.")
+
+# If the variable named "Age" is less than 20, the message "This person is a teenager." will be printed.
 elif Age < 20:
     print ("This person is a teenager.")
-elif Age < 65:
+
+# If the variable named "Age" is equal to or greater than 20 but less than 65, the message "This person is an adult." will be printed.
+elif Age >= 20 and Age < 65:
     print ("This person is an adult.")
+
+# If the variable named "Age" is equal to or greater than 65, the message "This person is an elder." will be printed.
 else:
     print ("This person is an elder.")
