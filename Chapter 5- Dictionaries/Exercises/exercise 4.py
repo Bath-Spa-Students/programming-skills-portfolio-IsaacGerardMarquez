2e9c6ed136fd3680331e5ebab93a2cd0dc609e22
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-15 13:29:07 +0400

    exercise 4.py

--- Chapter 5- Dictionaries/Exercises/exercise 4.py
+++ Chapter 5- Dictionaries/Exercises/exercise 4.py
@@ -3,23 +3,23 @@
 #Use a loop to print the name of each river included in the dictionary
 #Use a loop to print the name of each country included in the dictionary.
 
-# The dictionary contains three major rivers and the countries they each run through.
+# Creating a dictionary named "Major_Rivers" that contains three major rivers and the countries they each run through.
 Major_Rivers = {
     "Cagayan" : "Philippines",
     "Volga": "Russia",
     "Missouri": 'United States of America',
     }
 
-# Loop that prints (the river) runs through (the country).
+# Creating a loop that prints (the river) runs through (the country).
 for river, Country in Major_Rivers.items():
     print(f"The {river.title()} runs through {Country.title()}.")
 
-# Loop that prints the name of each river included in the dictionary.
+# Creating a loop that prints the name of each river included in the dictionary.
 print("\nThe following rivers are included in this data set:")
 for river in Major_Rivers.keys():
     print(f"- {river.title()}")
 
-# Loop that prints the name of each country included in the dictionary.
+# Creating a loop that prints the name of each country included in the dictionary.
 print("\nThe following countries are included in this data set:")
 for Country in Major_Rivers.values():
     print(f"- {Country.title()}")
