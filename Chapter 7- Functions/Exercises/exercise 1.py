475e448e1b8a404885c7ad1bbabac1ea9f799d47
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-15 15:29:12 +0400

    exercise 1.py

--- Chapter 7- Functions/Exercises/exercise 1.py
+++ Chapter 7- Functions/Exercises/exercise 1.py
@@ -2,9 +2,14 @@
 # Call the function, and make sure the message displays correctly.
 
 # Function that would print one sentence telling everyone what I am learning about in this chapter.
+# Defining a function and naming it "display_message".
 def display_message():
-    """Displaying or printing one sentence telling everyone what I am learning about in this chapter."""
+
+# Creating a variable named "My_Message" that stores a string value.
     My_Message = "In this chapter, I am learning about functions."
+    
+# Using the print() function to print or display the value of the variable named "My_Message".
     print(My_Message)
 
+# Executes the code inside the function.
 display_message()
