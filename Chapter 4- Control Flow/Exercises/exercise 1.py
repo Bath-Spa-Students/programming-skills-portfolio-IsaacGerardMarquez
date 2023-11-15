fe82d4512c743fc376433b25b80c37e22ca02343
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-15 11:48:32 +0400

    exercise 1.py

--- Chapter 4- Control Flow/Exercises/exercise 1.py
+++ Chapter 4- Control Flow/Exercises/exercise 1.py
@@ -3,13 +3,22 @@
 #•Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
 
 # Passed
+# The variable named "alien_color" is assigned to have the value of 'green'.
 alien_color = 'green'
 
+# Use "==" to determine if alien_color is equal to 'green'.
 if alien_color == 'green':
+
+# Because the condition is met (True), the if statement is executed, which is printing the message "Congratulations! You just earned 5 points!".
     print("Congratulations! You just earned 5 points!")
 
+
 # Failed
+# The variable named "alien_color" is assigned to have the value of 'yellow'.
 alien_color = 'yellow'
 
+# Use "==" to determine if alien_color is equal to 'green'.
 if alien_color == 'green':
+
+# Because the condition was not met (False), the if statement is not executed.
     print("Congratulations! You just earned 5 points!")
