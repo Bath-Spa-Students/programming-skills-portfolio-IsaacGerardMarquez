eb32d87390b8ca061a5d297f1832ec0a9f6fe669
Isaac Gerard <IsaacBathSpa@gmail.com>
2023-11-15 16:23:04 +0400

    exercise 4.py

--- Chapter 7- Functions/Exercises/exercise 4.py
+++ Chapter 7- Functions/Exercises/exercise 4.py
@@ -2,12 +2,19 @@
 # Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
 
 # Function that would print a large shirt and a medium shirt with the default message, I love Python.
+# Define the function named "make_shirt function."
+# The parameter named "size" has a default value of 'large' and the parameter named "message" has a default value of "I love Python.".
 def make_shirt(size='large', message="I love Python."):
-    """Summarizing the t shirt that I would be making."""
+
+# Creating a print() function to print the string "I made a {size} t-shirt.", where {size} is replaced with the value of the parameter named "size".
     print(f"\nI made a {size} t-shirt.")
+
+# Likewise, printing the string "The message printed on the shirt says, "{message}", where {message} is replaced with the value of the parameter named "message".
     print(f'The message printed on the shirt says, "{message}"')
 
+# Calls the function named "make_shirt" without any arguments.
 make_shirt()
+# Calls the function named "make_shirt" with the parameter named "size" set to "medium".
 make_shirt(size="medium")
-# Shirt with a different size and message.
+# Calls the function named "make_shirt" with both the parameters named "size" and "message" set.
 make_shirt("small", "Creative Computing - Group 2.")
